c69df383969b29b583163e66184c37ddb008126e
Justine <jjaycaneza@gmail.com>
2024-03-19 19:38:32 +0800

    feat: HCDC Settings

---
+++ herbert_app/herbert_app/doctype/hcdc_settings/test_hcdc_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, John Vincent Fiel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHCDCSettings(FrappeTestCase):
+	pass
